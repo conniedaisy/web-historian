12d1b3c4b7b61a25a7d02354d5b6f18d64f4f3d2
conniedaisy <conniedcheung@gmail.com>
2016-03-23 13:19:47 -0700

    Write to file url.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,13 +1,34 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
+var headers = require('./http-helpers.js').headers;
 // require more modules/folders here!
+
+var readPost = function(req, res, cb) {
+  var body = '';
+  console.log('receiving post');
+  req.on('data', function(chunk) {
+    body += chunk;
+  });
+  req.on('end', function() {
+    cb(body);
+  });
+};
 
 exports.handleRequest = function (req, res) {
   if (req.url === '/' && req.method === 'GET') {
+    console.log('getting index');
     var fileName = path.join(__dirname, 'public/index.html');
     var readStream = fs.createReadStream(fileName);
     readStream.pipe(res);
+  } else if (req.url === '/' && req.method === 'POST') {
+    console.log('something is here!');
+    res.writeHead(302, headers);
+    readPost(req, res, function(message) {
+      var url = message.split('=')[1];
+      fs.appendFileSync(archive.paths.list, url + '\n');
+    });
+    res.end('in process');
   } else {
     res.end(archive.paths.list);
   }
