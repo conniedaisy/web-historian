35d381a295eea1be2e55b96c0f04fd41a3200716
conniedaisy <conniedcheung@gmail.com>
2016-03-23 11:31:05 -0700

    Serve index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,14 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.url === '/' && req.method === 'GET') {
+    var fileName = path.join(__dirname, 'public/index.html');
+    var readStream = fs.createReadStream(fileName);
+    readStream.pipe(res);
+  } else {
+    res.end(archive.paths.list);
+  }
 };
