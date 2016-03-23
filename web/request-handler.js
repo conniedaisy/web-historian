286c03097f45c9f0324b2c3c07e6fbbc1c2262fa
conniedaisy <conniedcheung@gmail.com>
2016-03-23 14:43:12 -0700

    Return website from archive

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,7 +6,6 @@ var headers = require('./http-helpers.js').headers;
 
 var readPost = function(req, res, cb) {
   var body = '';
-  console.log('receiving post');
   req.on('data', function(chunk) {
     body += chunk;
   });
@@ -17,18 +16,34 @@ var readPost = function(req, res, cb) {
 
 exports.handleRequest = function (req, res) {
   if (req.url === '/' && req.method === 'GET') {
-    console.log('getting index');
+    console.log('test');
     var fileName = path.join(__dirname, 'public/index.html');
     var readStream = fs.createReadStream(fileName);
     readStream.pipe(res);
   } else if (req.url === '/' && req.method === 'POST') {
-    console.log('something is here!');
     res.writeHead(302, headers);
     readPost(req, res, function(message) {
       var url = message.split('=')[1];
       fs.appendFileSync(archive.paths.list, url + '\n');
     });
     res.end('in process');
+  } else if (req.method === 'GET') {
+    var url = req.url.slice(1);
+    console.log(url);
+    var filePath = archive.paths.archivedSites + '/' + url;
+
+    console.log(filePath);
+    // check if exits
+    fs.exists(filePath, function(exists) {
+      if (exists) {
+        res.writeHead(200);
+        var readStream = fs.createReadStream(filePath);
+        readStream.pipe(res);
+      } else {
+        res.writeHead(404, headers);
+        res.end('file not found');
+      }
+    });   
   } else {
     res.end(archive.paths.list);
   }
